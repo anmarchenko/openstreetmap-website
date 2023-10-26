ad99700c95d53e23c7ed3a269f2b8481b1618a3e
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2023-10-26 13:48:08 +0200

    running tests under docker

--- db/structure.sql
+++ db/structure.sql
@@ -109,7 +109,6 @@ CREATE TYPE public.user_status_enum AS ENUM (
 
 SET default_tablespace = '';
 
-SET default_table_access_method = heap;
 
 --
 -- Name: acls; Type: TABLE; Schema: public; Owner: -
